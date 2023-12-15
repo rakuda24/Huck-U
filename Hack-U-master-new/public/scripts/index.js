4b73a0591995536c2ddd1e48eb9924e8db069c1b
太田 <137127231+rakuda24@users.noreply.github.com>
2023-12-15 10:44:25 +0900

    Delete Hack-U-master-new/public directory

--- Hack-U-master-new/public/scripts/index.js
+++
@@ -1,9 +0,0 @@
-function newWindow() {
-    window.open('./create.html','create_tab','');
-}
-
-function createButton() {
-    let roomName = document.getElementById('text').value;
-    let roomText = document.getElementById('textarea').value;
-    location.href = "index.html?roomName="+roomName+"&roomText="+roomText;
-}
